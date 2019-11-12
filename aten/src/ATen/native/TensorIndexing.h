24c7e489482fa892b020104770a7105bd99a88f4
Will Feng <willfeng@fb.com>
2019-11-12 18:54:12 -0500

    C++ tensor advanced indexing: add SliceIndex / Slice / TensorIndex
    gh-metadata: pytorch pytorch 29633 gh/yf225/67/head

---
+++ aten/src/ATen/native/TensorIndexing.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <c10/util/Optional.h>
+#include <ATen/core/TensorBody.h>
+
+namespace at {
+namespace indexing {
+
+enum class SliceIndexType { None, Integer };
+enum class TensorIndexType { None, Ellipsis, Integer, Boolean, Slice, Tensor };
+
+struct CAFFE2_API NoneIndexType { NoneIndexType() {} };
+struct CAFFE2_API EllipsisIndexType { EllipsisIndexType() {} };
+
+CAFFE2_API extern const NoneIndexType None;
+CAFFE2_API extern const EllipsisIndexType Ellipsis;
+
+// yf225 TODO: can we use `c10::optional<int64_t>` and remove this class?
+struct CAFFE2_API SliceIndex {
+  SliceIndex(at::indexing::NoneIndexType none);
+  SliceIndex(int64_t integer);
+
+  bool is_none() const;
+  bool is_integer() const;
+  int64_t integer() const;
+
+ private:
+  int64_t integer_;
+  SliceIndexType type_;
+};
+
+struct CAFFE2_API Slice {
+ public:
+  Slice();
+  Slice(int64_t start, int64_t stop, int64_t step);
+
+  const int64_t& start() const;
+  const int64_t& stop() const;
+  const int64_t& step() const;
+
+ private:
+  int64_t start_;
+  int64_t stop_;
+  int64_t step_;
+};
+
+CAFFE2_API std::ostream& operator<<(std::ostream& stream, const Slice& slice);
+
+struct CAFFE2_API TensorIndex {
+  TensorIndex(at::indexing::NoneIndexType);
+  TensorIndex(at::indexing::EllipsisIndexType);
+  TensorIndex(const char *str);
+  TensorIndex(int64_t integer);
+  TensorIndex(int integer);
+  TensorIndex(bool boolean);
+  TensorIndex(std::initializer_list<SliceIndex> init_list);
+  TensorIndex(Tensor tensor);
+
+  bool is_none() const;
+  bool is_ellipsis() const;
+
+  bool is_integer() const;
+  int64_t integer() const;
+
+  bool is_boolean() const;
+  bool boolean() const;
+
+  bool is_slice() const;
+  const Slice& slice() const;
+
+  bool is_tensor() const;
+  const Tensor& tensor() const;
+
+ private:
+  int64_t integer_;
+  bool boolean_;
+  Slice slice_;
+  Tensor tensor_;
+  TensorIndexType type_;
+};
+
+CAFFE2_API std::ostream& operator<<(std::ostream& stream, const TensorIndex& tensor_index);
+CAFFE2_API std::ostream& operator<<(std::ostream& stream, const std::vector<TensorIndex>& tensor_indices);
+
+} // namespace indexing
+} // namespace at
